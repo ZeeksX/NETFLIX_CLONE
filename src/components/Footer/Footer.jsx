a186099ba5eb4086042be546bd9d54eec765a56f
ZeeksX <ikinwotezekiel@gmail.com>
2024-10-29 11:16:26 +0100

    added files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return <div className="footer"></div>;
+};
+
+export default Footer;
